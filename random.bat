9515a36f27c264dd8cd2d11443f5b1502b0ab607
Stephan Thierry <stephan.thierry@live.com>
2019-06-07 21:26:11 +0200

    Added LIST DISK to no-parm run Added support for calling with both start and end param

--- random.bat
+++ random.bat
@@ -1,11 +1,11 @@
 @echo off
-set "x=1"
+set "x=%1"
 :SymLoop
     echo select disk %x%
     set rng=%RANDOM%%RANDOM%
     set rng=%rng:~0,8%
     echo uniqueid disk id=%rng%
-    if "%x%"=="%1" goto :endLoop
+    if "%x%"=="%2" goto :endLoop
     SET /a "x+=1"
     GOTO :SymLoop
 :endLoop
--- run.bat
+++ run.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
 if "%1"=="" GOTO NBRMISSING
-Echo OK - Running on %1 drives
-call random %1  > random.diskpart
+if "%2"=="" GOTO NBRMISSING
+Echo OK - Running on DRIVES %1 to %2 drives
+call random %1 %2 > random.diskpart
 call diskpart /s random.diskpart
 GOTO END
 :NBRMISSING
 
 Echo Run [nbr_of_USB_drives]
-Echo Example: run 7 - Will start at drive 1 and set new unique ID for all drives up to and including drive 7
+Echo Example: run 1 7 - Will start at drive 1 and set new unique ID for all drives up to and including drive 7
+Echo.
+call DiskPart /s listdisk
 :END
