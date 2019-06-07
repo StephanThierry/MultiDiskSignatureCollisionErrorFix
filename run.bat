0883baf88e6778b98c56fd492bf2687d75c142ee
Stephan Thierry <stephan.thierry@live.com>
2019-06-07 21:27:57 +0200

    Fixed no-param comment

--- run.bat
+++ run.bat
@@ -7,8 +7,9 @@ call diskpart /s random.diskpart
 GOTO END
 :NBRMISSING
 
-Echo Run [nbr_of_USB_drives]
+Echo Run [start_disk] [end_disk]
 Echo Example: run 1 7 - Will start at drive 1 and set new unique ID for all drives up to and including drive 7
+Echo Example: run 4 4 - Will start and end at drive 4, running only on that drive
 Echo.
 call DiskPart /s listdisk
 :END
